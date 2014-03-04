b34c894440948c8905b4ff414ed863087ff4ab01
Jon Eyolfson <jon@eyl.io>
2014-03-03 21:22:14 -0500

    Added setup files

---
+++ setup.py
@@ -0,0 +1,29 @@
+# Copyright 2014 Jon Eyolfson
+#
+# This file is part of Django Gitolite.
+#
+# Django Gitolite is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# Django Gitolite is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# Django Gitolite. If not, see <http://www.gnu.org/licenses/>.
+
+from distutils.core import setup
+
+setup(
+    name = 'django-gitolite',
+    packages = ['django-gitolite'],
+    version = '0.0.1',
+    description = 'A basic Django app for using Gitolite',
+    author = 'Jon Eyolfson',
+    author_email = 'jon@eyl.io',
+    url = 'https://github.com/eyolfson/django-gitolite/',
+    download_url = ('https://github.com/eyolfson/django-gitolite/archive/'
+                    '0.0.1.tar.gz'),
+)
