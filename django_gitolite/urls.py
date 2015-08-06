aeffb8b37a70e9fbdb823ee8b6a05f20d8db6a73
Jon Eyolfson <jon@eyl.io>
2015-08-06 15:30:52 -0400

    Added underscores allowed in repo urls

--- django_gitolite/urls.py
+++ django_gitolite/urls.py
@@ -8,6 +8,6 @@ from django_gitolite import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<path>([A-Za-z0-9-+]+/)*[A-Za-z0-9-+]+)/$',
+    url(r'^(?P<path>([A-Za-z0-9-+_]+/)*[A-Za-z0-9-+_]+)/$',
         views.repo, name='repo'),
 ]
