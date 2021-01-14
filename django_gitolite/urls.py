a595e0e1381912b5428ccb5f2380bfaa6d6dd1ca
Jon Eyolfson <jon@eyl.io>
2021-01-14 17:16:26 -0500

    Update to Django 3+

--- django_gitolite/urls.py
+++ django_gitolite/urls.py
@@ -2,12 +2,12 @@
 #
 # This file is distributed under the GPLv3 license
 
-from django.conf.urls import url
+from django.urls import path
 
 from django_gitolite import views
 
+app_name = 'git'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<path>([A-Za-z0-9-+_]+/)*[A-Za-z0-9-+_]+)/$',
-        views.repo, name='repo'),
+    path('', views.index, name='index'),
+    path('<path:path>/', views.repo, name='repo'),
 ]
